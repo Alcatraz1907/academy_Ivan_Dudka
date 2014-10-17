4b429b5e1e6d870182e48f2ec840662a67af4ec3
Alcatraz1907 <AlcatrazUa521@gmail.com>
2014-10-17 19:40:06 +0300

    Revert "#Dudka"
    This reverts commit f73a5a2900b2d08e021ba022cdf99670cafd54c7.

--- www/units/header.php
+++
@@ -1,5 +0,0 @@
-<link href="../style1.css" rel="stylesheet" type="text/css">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<div class="textOfHeader"> Лабораторні роботи з PHP</div>
-
